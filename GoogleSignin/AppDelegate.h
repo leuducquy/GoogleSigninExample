91c2ae2051802dc60a70df273f5acee8c84304c0
Quy <quy@Quys-Mac-mini.local>
2016-12-16 11:05:55 +0700

    Initial Commit

---
+++ GoogleSignin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleSignin
+//
+//  Created by Quy on 12/16/16.
+//  Copyright © 2016 Quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleSignin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleSignin
+//
+//  Created by Quy on 12/16/16.
+//  Copyright © 2016 Quy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
